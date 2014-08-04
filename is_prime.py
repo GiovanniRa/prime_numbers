8252919ca6a5ac5b8926a1e4dd4fa17d0911008a
GiovanniRa <GiovanniRa@users.noreply.github.com>
2014-08-04 17:52:01 +0200

    Create is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,23 @@
+def is_prime2(n):
+    
+    x = True
+
+    if n == 1:
+        x = False
+
+    else:
+        for i in range(2, n ):
+            if n % i == 0:
+                x = False
+                break
+            else:
+                x = True
+
+    if x:
+        print n
+
+
+
+
+
+
